812ff4d6ae61496d09a5688f64ebb5b9184b965d
Roemer Hendrikx <roemba@pm.me>
2021-07-30 16:13:30 +0200

    Changed main args to Revive

--- app/src/main/kotlin/roemer/rebalancing/main.kt
+++ app/src/main/kotlin/roemer/rebalancing/main.kt
@@ -7,10 +7,17 @@ import roemer.revive.LpSolveDemo
 fun main(args: Array<String>) {
     println("Program has started")
 
-    //val graph = GraphHolder("difficult_graph.txt", NodeTypes.CoinWasher)
-    val graph = GraphHolder("nodes_05-05-2021.json", "channels_05-05-2021.json", NodeTypes.CoinWasher)
+    // val tree1 = MerkleTree(listOf("test11", "test2", "test3", "test4", "test5"))
+    // val tree2 = MerkleTree(listOf("test11", "test2", "test3", "test4", "test5"))
+    // val tree1Dig = tree1.digest()
+    // val tree2Dig = tree2.digest()
 
-    val algoSettings = mapOf("hopCount" to 3, "maxNumberOfInvites" to 20, "percentageOfLeaders" to 0.1F)
+    // assert(tree1Dig contentEquals tree2Dig)
+
+    val graph = GraphHolder("difficult_graph.txt", NodeTypes.Revive)
+    //val graph = GraphHolder("nodes_05-05-2021.json", "channels_05-05-2021.json", NodeTypes.CoinWasher)
+
+    val algoSettings = mapOf("hopCount" to 3, "maxNumberOfInvites" to 20, "percentageOfLeaders" to 1F)
     graph.start(algoSettings)
     // LpSolveDemo().demo()
 }
